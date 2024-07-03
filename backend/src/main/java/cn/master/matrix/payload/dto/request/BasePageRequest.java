b31198cb2cbe842aec48e6853e5a16370bcfa5f1
lijinglin <the2ndindec@gmail.com>
2024-07-03 17:17:05 +0800

    fix: 未获取到参数问题

--- backend/src/main/java/cn/master/matrix/payload/dto/request/BasePageRequest.java
+++ backend/src/main/java/cn/master/matrix/payload/dto/request/BasePageRequest.java
@@ -1,6 +1,7 @@
 package cn.master.matrix.payload.dto.request;
 
 import cn.master.matrix.payload.dto.BaseCondition;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -15,10 +16,12 @@ import lombok.EqualsAndHashCode;
 public class BasePageRequest extends BaseCondition {
     @Min(value = 1, message = "当前页码必须大于0")
     @Schema(description = "当前页码")
+    @JsonProperty(value = "pageNum")
     private int pageNum;
 
     @Min(value = 5, message = "每页显示条数必须不小于5")
     @Max(value = 500, message = "每页显示条数不能大于500")
     @Schema(description = "每页显示条数")
+    @JsonProperty(value = "pageSize")
     private int pageSize;
 }
