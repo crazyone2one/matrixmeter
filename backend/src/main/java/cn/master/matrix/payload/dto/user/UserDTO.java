987a8362e6c31fec833725c34101b82c94776ea4
lijinglin <the2ndindec@gmail.com>
2024-07-05 14:12:59 +0800

    refactor(登录接口): Refactor JwtResponse to extend UserDTO and simplify token response construction.

--- backend/src/main/java/cn/master/matrix/payload/dto/user/UserDTO.java
+++ backend/src/main/java/cn/master/matrix/payload/dto/user/UserDTO.java
@@ -1,10 +1,8 @@
 package cn.master.matrix.payload.dto.user;
 
-import cn.master.matrix.entity.User;
 import cn.master.matrix.entity.UserRole;
 import cn.master.matrix.entity.UserRoleRelation;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +10,14 @@ import java.util.List;
 /**
  * @author Created by 11's papa on 06/28/2024
  **/
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class UserDTO extends User {
+public class UserDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String lastOrganizationId;
+    private String lastProjectId;
+    private Boolean enable;
     private List<UserRole> userRoles = new ArrayList<>();
     private List<UserRoleRelation> userRoleRelations = new ArrayList<>();
     private List<UserRoleResourceDTO> userRolePermissions = new ArrayList<>();
